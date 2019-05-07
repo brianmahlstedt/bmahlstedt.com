d7158b1c4285969732d0acd8c88db103741e8ddf
Brian Mahlstedt <brian.mahlstedt@gmail.com>
2019-05-07 21:23:18 +0000

    works

--- bmahlstedt/__init__.py
+++ bmahlstedt/__init__.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<center>GO NINERS</center>'
+    return '<img src="https://i.ebayimg.com/images/g/ApsAAOSwJEpcp3zC/s-l300.jpg" alt="Niners Helmet" style="margin: 10px auto; display: block;">'
