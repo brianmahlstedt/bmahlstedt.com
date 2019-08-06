4d2c7bd0c14df40ef3e3bef95e43e9927a025457
Brian Mahlstedt <brian.mahlstedt@gmail.com>
2019-08-06 15:40:31 -0700

    #5 just making the navbar button class part of NavLink

--- client/src/components/MyNav/index.jsx
+++ client/src/components/MyNav/index.jsx
@@ -4,21 +4,15 @@ import { NavLink } from 'react-router-dom';
 function MyNav() {
     return (
         <div className="row align-items-center justify-content-center">
-            <button type="button" className="btn btn-light">
-                <NavLink exact activeClassName="btn btn-dark" to="/">
-                    Home
-                </NavLink>
-            </button>
-            <button type="button" className="btn btn-light">
-                <NavLink activeClassName="btn btn-dark" to="/logos">
-                    Teams
-                </NavLink>
-            </button>
-            <button type="button" className="btn btn-light">
-                <NavLink activeClassName="btn btn-dark" to="/blog">
-                    Blog
-                </NavLink>
-            </button>
+            <NavLink exact className="btn btn-light" activeClassName="btn btn-dark" to="/">
+                Home
+            </NavLink>
+            <NavLink className="btn btn-light" activeClassName="btn btn-dark" to="/logos">
+                Teams
+            </NavLink>
+            <NavLink className="btn btn-light" activeClassName="btn btn-dark" to="/blog">
+                Blog
+            </NavLink>
         </div>
     );
 }
