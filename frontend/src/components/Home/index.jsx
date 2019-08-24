8f13d71a05293cf4e946bd885e35dfc649dfd459
Brian Mahlstedt <brian.mahlstedt@gmail.com>
2019-08-23 18:21:21 -0700

    #9 starting backend with pm2 now, and added linkedin/github icons

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Home() {
     return (
-        <div className="container pt-5">
-            <h1 className="text-center">Welcome</h1>
+        <div className="text-center">
+            <h1>Welcome</h1>
+            <a className="px-3" href="https://www.linkedin.com/in/brianmahlstedt">
+                <img height="64" width="64" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/linkedin.svg" />
+            </a>
+            <a className="px-3 mt-5" href="https://github.com/brianmahlstedt">
+                <img height="64" width="64" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/github.svg" />
+            </a>
         </div>
     );
 }
